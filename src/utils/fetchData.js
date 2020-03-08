a45d943e994dc03d147c1a0f83126e1161ec185a
Nicole Ordoqui Morales <nicole.ordoqui@gmail.com>
2020-03-08 17:29:29 -0300

    Promises challenge

---
+++ src/utils/fetchData.js
@@ -0,0 +1,20 @@
+const XMLHpptRequest = require('xmlhttprequest').XMLHttpRequest;
+ 
+const  fetchData = (url_api) => {
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHpptRequest(); 
+        xhttp.open('GET', url_api, true);
+        
+        xhttp.onreadystatechange = (() => { 
+            if(xhttp.readyState === 4) {
+                (xhttp.status === 200)
+                    ? resolve(JSON.parse(xhttp.responseText))
+                    : reject(new Error(`Error ${ url_api }`, url_api))    
+            }
+        });
+
+        xhttp.send();
+    });
+}
+
+module.exports = fetchData;
