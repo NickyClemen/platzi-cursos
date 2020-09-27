e9bff7923116356d482c3a2bc297ab08aa8fea69
nickyclemen <nicole.ordoqui@gmail.com>
2020-09-27 17:52:20 -0300

    Restaurar index.js

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('./routes');
-
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use('/api', router);
-
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000.'));
