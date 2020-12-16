a0e4ab93b28681a13b09cbec2f05a2c4d679614f
Nicole Ordoqui Morales <nicole.ordoqui@gmail.com>
2020-12-16 16:50:15 -0300

    Actualizado tests/agent.tests.js

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -1,18 +1,47 @@
 const test = require('ava');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
 
 let config = {
     logging: () => {}
+}
+
+let agentStub = null;
+let metricStub = {
+    belongsTo: sinon.spy(), // Función específica que permite obtener información de los tests.
 };
 
 let db = null;
+let sandbox = null;
 
 test.beforeEach(async () => {
-    const setupDatabase = require('../');
+    sandbox = sinon.createSandbox();
+
+    agentStub = {
+        hasMany: sandbox.spy(),
+    };
+
+    const setupDatabase = proxyquire('../', {
+        './models/agent': () => agentStub,
+        './models/metric': () => metricStub,
+    });
+
     db = await setupDatabase(config);
+});
+
+test.afterEach(() => {
+    sandbox && sandbox.restore();
 });
 
 test('agent', t => {
     t.truthy(db.agent, 'Agent service should exist.');
+});
+
+test.serial('setup', t => {
+    t.true(agentStub.hasMany.called, 'agentModel.hasMany was executed.');
+    t.true(agentStub.hasMany.calledWith(metricStub), 'argument should be the metric model.');
+    t.true(metricStub.belongsTo.called, 'metricStub.belongsTo was executed.')
+    t.true(metricStub.belongsTo.calledWith(agentStub), 'argument should be the agent model.')
 });
 
 /* Strub: Muy similar al objeto original (al modelo), pero que la respuesta y los argumentos
