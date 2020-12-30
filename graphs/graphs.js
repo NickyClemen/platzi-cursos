5f156103fd3a88bf1a211924ce8ad4ca2c465895
Nicole Ordoqui Morales <nicole.ordoqui@gmail.com>
2020-12-30 13:26:30 -0300

    Curso terminado.

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -14,10 +14,54 @@
 
     Forma de representar los graphs
         a) Edge list: Un array list. Representan las conexiones entre los nodos.
-            const grapth = [
+            const graph = [
                 [0, 2],
                 [2, 3],
                 [2, 1],
                 [1, 3],
             ];
-        b) Adjancent list: Se pueden utilizar otros tipo de datos.   */
+        b) Adjacent (JSON) List: Se pueden utilizar otros tipo de datos.
+            const graph = [
+                [2], [2, 3], [0, 1, 3], [1, 2],
+            ]; -> Un array con arrays internos que muestra las conexiones por índices.
+
+            const graph = {
+                0: [2],
+                1: [2, 3],
+                2: [0, 1, 3],
+                3: [1, 2],
+            }; -> Es más fácil organizarlo visualmente.
+
+        c) Adjacent (JSON) Matrix
+            const graph = [
+                [0, 0, 1, 0],
+                [0, 0, 1, 1],
+                [1, 1, 0, 1],
+                [0, 1, 1, 0],
+            ]; -> Se representa con cero y uno (muy parecido al binario).
+            Si existe una conexión, se coloca 1. Si no existe, se coloca 0.
+
+            const graph = {
+                0: [0, 0, 1, 0],
+                1: [0, 0, 1, 1],
+                2: [1, 1, 0, 1],
+                3: [0, 1, 1, 0],
+            }; */
+class Graph {
+    constructor() {
+        this.nodes = 0;
+        this.adjacentList = {};
+    }
+
+    addVertex(node) {
+        this.adjacentList[node] = [];
+        this.nodes++;
+    }
+
+    addEdge(nodeOne, nodeTwo) {
+        this.adjacentList[nodeOne].push(nodeTwo);
+        this.adjacentList[nodeTwo].push(nodeOne);
+    }
+}
+
+const myGraph = new Graph();
